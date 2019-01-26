f8bd071c4a868561ad0659621bae38b319b70739
Ericsson Budhilaw <ericsson@Ericssons-MacBook-Pro.local>
2019-01-26 18:05:18 +0700

    Fix for docker support

--- config/database.php
+++ config/database.php
@@ -9,15 +9,15 @@ return [
             * Uncomment this below
             * if you want to use with Docker
             */
-            'database' => env('DB_MONGO_DATABASE', 'lumenblog'),
-            'dsn' => env('DB_MONGO_DSN', 'mongodb://mongosrv:27017'),
+            // 'database' => env('DB_MONGO_DATABASE', 'lumenblog'),
+            // 'dsn' => env('DB_MONGO_DSN', 'mongodb://mongosrv:27017'),
 
-            // 'driver' => 'mongodb',
-            // 'host' => env('DB_HOST', '127.0.0.1'),
-            // 'port' => env('DB_PORT', 27017),
-            // 'database' => env('DB_DATABASE'),
-            // 'username' => env('DB_USERNAME'),
-            // 'password' => env('DB_PASSWORD'),
+            'driver' => 'mongodb',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', 27017),
+            'database' => env('DB_DATABASE'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
             'options' => [
                 'database' => 'admin' // sets the authentication database required by mongo 3
             ]
